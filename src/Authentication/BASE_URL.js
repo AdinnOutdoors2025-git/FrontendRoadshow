3c43a569599188e04b2d80b7f360d5474eff58a1
AdinnOutdoor <reactdeveloper@adinn.co.in>
2025-10-29 11:12:37 +0530

    roadshow newfrontend

--- src/Authentication/BASE_URL.js
+++ src/Authentication/BASE_URL.js
@@ -1,3 +1,2 @@
-// export const baseUrl = "https://backend-bq11.onrender.com"; 
 // export const baseUrl = "http://192.168.2.159:3001";
 export const baseUrl = "https://backendroadshow.onrender.com";
