b2b925ac1f28e7dc6df3c3ef183b60b806dcdb29
AdinnOutdoor <reactdeveloper@adinn.co.in>
2025-10-30 15:21:31 +0530

    roadshow frontendNew

--- src/AdminPanel/ad1VehicleUpload.jsx
+++ src/AdminPanel/ad1VehicleUpload.jsx
@@ -3673,15 +3673,15 @@ function VehicleUpload() {
                                             value={vehicleRating}
                                             onChange={(e) => handleRatingChange(e.target.value)}
                                         >
-                                            <option value="1">1</option>
+                                            <option value="1">1.0</option>
                                             <option value="1.5">1.5</option>
-                                            <option value="2">2</option>
+                                            <option value="2">2.0</option>
                                             <option value="2.5">2.5</option>
-                                            <option value="3">3</option>
+                                            <option value="3">3.0</option>
                                             <option value="3.5">3.5</option>
-                                            <option value="4">4</option>
+                                            <option value="4">4.0</option>
                                             <option value="4.5">4.5</option>
-                                            <option value="5">5</option>
+                                            <option value="5">5.0</option>
                                         </select>
                                     </div>
                                 </div>
