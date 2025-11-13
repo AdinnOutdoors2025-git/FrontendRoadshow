a519f84e4089da1349b8bc30474c24bfc6bd6c57
AdinnDigital2025 <adinndigitalservices@gmail.com>
2025-11-13 15:48:41 +0530

    Web-Dev: baseurl local update

--- src/Authentication/BASE_URL.js
+++ src/Authentication/BASE_URL.js
@@ -1,2 +1,5 @@
 // export const baseUrl = "http://192.168.2.159:3001";
-export const baseUrl = "https://backendroadshow.onrender.com";
+export const baseUrl = "https://backendroadshow-1.onrender.com";
+
+
+// https://backendroadshow.onrender.com
