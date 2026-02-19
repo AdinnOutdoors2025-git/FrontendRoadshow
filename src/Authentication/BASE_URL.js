13485653a8881ed353b2ac16567721f2be6d7df1
Sathishkumar Dhanasekaran <srbedev@adinn.co.in>
2026-02-19 13:24:06 +0530

    rm local files

--- src/Authentication/BASE_URL.js
+++ src/Authentication/BASE_URL.js
@@ -2,6 +2,7 @@
 // export const baseUrl = "http://192.168.2.204:3001";
 //  export const baseUrl = "https://backendroadshow-1.onrender.com";
  export const baseUrl = "https://backendroadshow.onrender.com";
+//  #export const baseUrl = "http://localhost:3001";
 
 
 
